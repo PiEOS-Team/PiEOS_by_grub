519e1912d2049e8664a1bea65cb77ab94506a06b
苦麒麟 <kuqilin_1@163.com>
2023-12-03 09:35:59 +0800

    Add files via upload

---
+++ include/types.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.h
+ *
+ *    Description:  一些类型的定义
+ *
+ *        Version:  0.0.1 Alaph
+ *        Created:  2023年12月03日 09时07分49秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  PiEOS-Team
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef INCLUDE_TYPES_H_
+#define INCLUDE_TYPES_H_
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+typedef unsigned int    uint32_t;
+typedef           int    int32_t;
+typedef unsigned short uint16_t;
+typedef           short int16_t;
+typedef unsigned char  uint8_t;
+typedef           char  int8_t;
+
+#endif
