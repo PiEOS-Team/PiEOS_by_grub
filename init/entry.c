d068ec14b6b4c858c7e9a626fe2404acb57f1a6c
苦麒麟 <kuqilin_1@163.com>
2023-12-10 09:28:01 +0800

    0.0.3Alaph

--- init/entry.c
+++ init/entry.c
@@ -19,27 +19,7 @@
 #include "types.h"
 
 int kern_entry(){
-    uint8_t *input = (uint8_t *)0xB8000;
-    uint8_t color = (0 << 4) | (15 & 0x0F);
-
-	*input++ = 'H'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'o'; *input++ = color;
-	*input++ = ','; *input++ = color;
-	*input++ = ' '; *input++ = color;
-	*input++ = 'P'; *input++ = color;
-	*input++ = 'i'; *input++ = color;
-	*input++ = 'E'; *input++ = color;
-	*input++ = 'O'; *input++ = color;
-	*input++ = 'S'; *input++ = color;
-	*input++ = '-'; *input++ = color;
-	*input++ = 'T'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'a'; *input++ = color;
-	*input++ = 'm'; *input++ = color;
-	*input++ = '!'; *input++ = color;
-
+	console_clear();
+	console_write_color("Hello, PiEOS!\n", rc_black, rc_green);
 	return 0;
 }
