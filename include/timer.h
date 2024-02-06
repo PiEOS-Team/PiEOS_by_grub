aa40389f3c7807602c886cc8c06f0bd7d1f880c3
苦麒麟 <kuqilin_1@163.com>
2024-02-06 22:23:36 +0800

    0.0.10

---
+++ include/timer.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timer.c
+ *
+ *    Description:  定义 PIT(周期中断定时器) 相关函数
+ *
+ *        Version:  0.0.10Alpha
+ *        Created:  2024年2月6日 20时11分10秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  PiEOS-Team
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_TIMER_H_
+#define INCLUDE_TIMER_H_
+
+#include "types.h"
+
+void init_timer(uint32_t frequency);
+
+#endif
