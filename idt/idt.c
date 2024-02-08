34e9e83313253f0d5831da0ba9337ec913490c69
苦麒麟 <kuqilin_1@163.com>
2024-02-08 20:47:30 +0800

    0.1

--- idt/idt.c
+++ idt/idt.c
@@ -5,7 +5,7 @@
  *
  *    Description:  
  *
- *        Version:  0.0.9Alpha
+ *        Version:  0.1
  *        Created:  2024年2月6日 16时51分19秒
  *       Revision:  none
  *       Compiler:  gcc
@@ -36,55 +36,55 @@ static void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags
 extern void idt_flush(uint32_t);
 
 // 初始化中断描述符表
-void init_idt(){
-    // 重新映射 IQR 表
-    // 两片级联的 Intel 8259A 芯片
-    // 主片端口 0x20 0x21
-    // 从片端口 0xA0 0xA1
-    
-    // 初始化主片、从片
-    // 0001 0001
-    outb(0x20, 0x11);
-    outb(0xA0, 0x11);
-    
-    // 设置主片 IQR 从 0x20(32) 号中断开始
-    outb(0x21, 0x20);
-    
-    // 设置从片 IQR 从 0x28(40) 号中断开始
-    outb(0xA1, 0x28);
-    
-    // 设置主片 IR2 引脚连接从片
-    outb(0x21, 0x04);
-    
-    // 告诉从片输出引脚和主片 IR2 号相连
-    outb(0xA1, 0x02);
-    
-    // 设置主片和从片按照 8086 的方式工作
-    outb(0x21, 0x01);
-    outb(0xA1, 0x01);
-    
-    // 设置主从片允许中断
-    outb(0x21, 0x0);
-    outb(0xA1, 0x0);
-    
+void init_idt(){	
+	// 重新映射 IRQ 表
+	// 两片级联的 Intel 8259A 芯片
+	// 主片端口 0x20 0x21
+	// 从片端口 0xA0 0xA1
+	
+	// 初始化主片、从片
+	// 0001 0001
+	outb(0x20, 0x11);
+	outb(0xA0, 0x11);
+
+	// 设置主片 IRQ 从 0x20(32) 号中断开始
+	outb(0x21, 0x20);
+
+	// 设置从片 IRQ 从 0x28(40) 号中断开始
+	outb(0xA1, 0x28);
+	
+	// 设置主片 IR2 引脚连接从片
+	outb(0x21, 0x04);
+
+	// 告诉从片输出引脚和主片 IR2 号相连
+	outb(0xA1, 0x02);
+	
+	// 设置主片和从片按照 8086 的方式工作
+	outb(0x21, 0x01);
+	outb(0xA1, 0x01);
+	
+	// 设置主从片允许中断
+	outb(0x21, 0x0);
+	outb(0xA1, 0x0);
+
 	bzero((uint8_t *)&interrupt_handlers, sizeof(interrupt_handler_t) * 256);
 	
 	idt_ptr.limit = sizeof(idt_entry_t) * 256 - 1;
-	idt_ptr.base  = (uint32_t)&idt_entries;
+	idt_ptr.base = (uint32_t)&idt_entries;
 	
 	bzero((uint8_t *)&idt_entries, sizeof(idt_entry_t) * 256);
 
 	// 0-32:  用于 CPU 的中断处理
-	idt_set_gate( 0, (uint32_t)isr0, 0x08, 0x8E);
-	idt_set_gate( 1, (uint32_t)isr1, 0x08, 0x8E);
-	idt_set_gate( 2, (uint32_t)isr2, 0x08, 0x8E);
-	idt_set_gate( 3, (uint32_t)isr3, 0x08, 0x8E);
-	idt_set_gate( 4, (uint32_t)isr4, 0x08, 0x8E);
-	idt_set_gate( 5, (uint32_t)isr5, 0x08, 0x8E);
-	idt_set_gate( 6, (uint32_t)isr6, 0x08, 0x8E);
-	idt_set_gate( 7, (uint32_t)isr7, 0x08, 0x8E);
-	idt_set_gate( 8, (uint32_t)isr8, 0x08, 0x8E);
-	idt_set_gate( 9, (uint32_t)isr9, 0x08, 0x8E);
+	idt_set_gate(0, (uint32_t)isr0, 0x08, 0x8E);
+	idt_set_gate(1, (uint32_t)isr1, 0x08, 0x8E);
+	idt_set_gate(2, (uint32_t)isr2, 0x08, 0x8E);
+	idt_set_gate(3, (uint32_t)isr3, 0x08, 0x8E);
+	idt_set_gate(4, (uint32_t)isr4, 0x08, 0x8E);
+	idt_set_gate(5, (uint32_t)isr5, 0x08, 0x8E);
+	idt_set_gate(6, (uint32_t)isr6, 0x08, 0x8E);
+	idt_set_gate(7, (uint32_t)isr7, 0x08, 0x8E);
+	idt_set_gate(8, (uint32_t)isr8, 0x08, 0x8E);
+	idt_set_gate(9, (uint32_t)isr9, 0x08, 0x8E);
 	idt_set_gate(10, (uint32_t)isr10, 0x08, 0x8E);
 	idt_set_gate(11, (uint32_t)isr11, 0x08, 0x8E);
 	idt_set_gate(12, (uint32_t)isr12, 0x08, 0x8E);
@@ -107,7 +107,7 @@ void init_idt(){
 	idt_set_gate(29, (uint32_t)isr29, 0x08, 0x8E);
 	idt_set_gate(30, (uint32_t)isr30, 0x08, 0x8E);
 	idt_set_gate(31, (uint32_t)isr31, 0x08, 0x8E);
-	
+
 	idt_set_gate(32, (uint32_t)irq0, 0x08, 0x8E);
 	idt_set_gate(33, (uint32_t)irq1, 0x08, 0x8E);
 	idt_set_gate(34, (uint32_t)irq2, 0x08, 0x8E);
@@ -142,14 +142,13 @@ static void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags
 
 	// 先留下 0x60 这个魔数，以后实现用户态时候
 	// 这个与运算可以设置中断门的特权级别为 3
-	idt_entries[num].flags = flags;    // | 0x60
+	idt_entries[num].flags = flags;  // | 0x60
 }
 
 // 调用中断处理函数
-void isr_handler(pt_regs *regs)
-{
+void isr_handler(pt_regs *regs){
 	if (interrupt_handlers[regs->int_no]){
-	      interrupt_handlers[regs->int_no](regs);
+	    interrupt_handlers[regs->int_no](regs);
 	}
 	else{
 		printk_color(rc_black, rc_blue, "Unhandled interrupt: %d\n", regs->int_no);
@@ -162,8 +161,7 @@ void register_interrupt_handler(uint8_t n, interrupt_handler_t h){
 }
 
 // IRQ 处理函数
-void irq_handler(pt_regs *regs)
-{
+void irq_handler(pt_regs *regs){
 	// 发送中断结束信号给 PICs
 	// 按照我们的设置，从 32 号中断起为用户自定义中断
 	// 因为单片的 Intel 8259A 芯片只能处理 8 级中断
